5703e6373d17fd0e8d72cf81d4af10d751285c61
BlendianLatifi <93226607+BlendianLatifi@users.noreply.github.com>
2026-03-18 14:11:32 +0100

    Dummy

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ console.log("Hello World");
 //git init
 //git add remote origin...
 //git branch -m main
+
+//we have pushed the code on main branch
+const dummy = () => {
+    console.log("Dummy");
+}
